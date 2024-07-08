59d15e5e12a95b9c8a9d470fdac8ae3d0ddf72d1
sudhanshu-niva <sudhanshu@nivamoney.com>
2024-07-08 20:30:00 +0530

    terms page

---
+++ src/app/Terms/page.tsx
@@ -0,0 +1,15 @@
+import PrivacyComponent from "@/components/PrivacyComponent/PrivacyComponent";
+import TermsComponent from "@/components/TermsComponent/TermsComponent";
+
+
+const TermsPage: React.FC = () => {
+  return (
+    <>
+      <main>
+        <TermsComponent/>
+      </main>
+    </>
+  );
+};
+
+export default TermsPage;
