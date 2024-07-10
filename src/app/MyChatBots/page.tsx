e99cede4ef92c58ae8b286e50f6ad347641ab945
sneha-jhaveri <sneha@nivamoney.com>
2024-07-10 15:54:33 +0530

    redesigned mychatbot page

---
+++ src/app/MyChatBots/page.tsx
@@ -0,0 +1,15 @@
+import ChatBotList from "@/components/ChatBot/ChatbotList";
+import React from "react";
+
+
+const MyChatBots: React.FC = () => {
+  return (
+    <>
+      <main>
+        <ChatBotList />
+      </main>
+    </>
+  );
+};
+
+export default MyChatBots;
