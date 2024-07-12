c3926d26c1544be3db7a3c6eee7e3b69a296fb8c
Kalpanathmajhi <71187581+Kalpanathmajhi@users.noreply.github.com>
2024-07-12 17:37:47 +0530

    added faq changes

--- src/app/faq/faqdata.tsx
+++ src/app/faq/faqdata.tsx
@@ -1,15 +1,8 @@
 // components/FAQ.tsx
 import React, { useState } from 'react';
 import FAQbody from './page';
-
-
-
 const FAQ = () => {
   const [expandedPanel, setExpandedPanel] = useState<string | false>('panel1');
-
- 
-  
-
   return (
     <>
       <FAQbody />
