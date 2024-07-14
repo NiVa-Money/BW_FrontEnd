3cd44213f161274945882a4e7c3f5c6a55e0f4aa
sneha-jhaveri <sneha@nivamoney.com>
2024-07-14 19:33:39 +0530

    added new chat screen

---
+++ src/app/newchat/page.tsx
@@ -0,0 +1,14 @@
+import NewChatComponent from "@/components/NewChat/NewChat";
+import React from "react";
+
+const NewChat: React.FC = () => {
+  return (
+    <>
+      <main>
+        <NewChatComponent />
+      </main>
+    </>
+  );
+};
+
+export default NewChat;
