7cb736bb2bf293039afe1c9ef31ce3de1ca31083
sudhanshu-niva <sudhanshu@nivamoney.com>
2024-08-01 18:21:36 +0530

    state update

--- src/redux/reducers/paymentReducers.ts
+++ src/redux/reducers/paymentReducers.ts
@@ -1,20 +1,16 @@
-const initialState = {
-    orderId: null,
-    loading: false,
-    error: null,
-  };
-  
-  const paymentReducer = (state = initialState, action: any) => {
-    switch (action.type) {
-      case 'CREATE_PAYMENT_REQUEST':
-        return { ...state, loading: true };
-      case 'CREATE_PAYMENT_SUCCESS':
-        return { ...state, loading: false, orderId: action.payload };
-      case 'CREATE_PAYMENT_FAILURE':
-        return { ...state, loading: false, error: action.payload };
-      default:
-        return state;
-    }
-  };
-  
-  export default paymentReducer;
+import initialState from './initialState';
+
+const paymentReducer = (state = initialState.payment, action: any) => {
+  switch (action.type) {
+    case 'CREATE_PAYMENT_REQUEST':
+      return { ...state, loading: true };
+    case 'CREATE_PAYMENT_SUCCESS':
+      return { ...state, loading: false, orderId: action.payload };
+    case 'CREATE_PAYMENT_FAILURE':
+      return { ...state, loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
+
+export default paymentReducer;
