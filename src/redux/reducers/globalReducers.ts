f2f78dcfdefc3df4b389eb0a4df66f9f86bca330
kalpanathmajhi <kalpanathmajhi@gmail.com>
2024-08-02 02:08:17 +0530

    added fix

--- src/redux/reducers/globalReducers.ts
+++ src/redux/reducers/globalReducers.ts
@@ -77,6 +77,7 @@ export default function globalReducers(state = initialState.root, action: any) {
       return {
         ...state,
         GLoginData: action.payload,
+        googleLogin: true,
         loader:false
       };
 
@@ -84,6 +85,7 @@ export default function globalReducers(state = initialState.root, action: any) {
         return {
           ...state,
           GLoginData: action.payload,
+          googleLogin: false,
           loader:false
         };
 
