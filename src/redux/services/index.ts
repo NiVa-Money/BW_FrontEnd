0ff6e85d84d4f7ec74a10b2918c31d04291d9be4
sneha-jhaveri <sneha@nivamoney.com>
2024-08-03 03:22:42 +0530

    revamped code

--- src/redux/services/index.ts
+++ src/redux/services/index.ts
@@ -289,8 +289,7 @@ export const getAdvanceFeatureService = async (payload: any) => {
 export const processPayPalPaymentService = async (payload : any) => {
   try {
     const response = await axiosInstance.post(`/payment/create`, payload);
-    console.log('payload of payment' , payload)
-    console.log('response of payment' , response.data)
+    console.log('response of payment' , payload.response.data)
     return response.data;
   } catch (error) {
     throw new Error('Payment processing failed');
@@ -299,6 +298,8 @@ export const processPayPalPaymentService = async (payload : any) => {
 
 
 export const capturePaymentService = async (_id: string) => {
+  console.log('response of capture' , _id)
   const response = await axiosInstance.post(`/payment/capture/${_id}`);
+  console.log('response of capture' , response.data)
   return response.data;
 };
