28582e277849c3997feeddd739454cb36c9edcf0
sneha-jhaveri <sneha@nivamoney.com>
2024-08-08 16:07:16 +0530

    added few changes

--- src/components/Pricing/index.tsx
+++ src/components/Pricing/index.tsx
@@ -148,51 +148,52 @@ const PricingCard = () => {
     {
       title: 'Basic',
       price: '0.00',
-      sessions: '100 Sessions',
+      sessions: '100 Messages',
       features: [
         'Access to essential features for creating your AI chatbot.',
-        'Suitable for up to 100 chat messages per month.',
+        'Suitable for up to 100 chat messages.',
         'Manage 1 Bot Profile with 1 Knowledge Base.',
         'Text uploads allowed for content.',
       ],
-      backgroundColor: 'bg-emerald-400',
+      // backgroundColor: 'bg-emerald-400',
+      backgroundColor: 'bg-pink-400',
     },
     {
-      title: 'BotWot Exclusive',
-      price: '4.99',
-      sessions: '10,000 Sessions',
+      title: 'BotWot Starter',
+      price: '29.99',
+      sessions: '10,000 Messages',
       features: [
-        'Advanced tools for building and managing your chatbot.',
-        'Suitable for up to 10,000 chat messages per month.',
+        "Advanced tools to create and manage your chatbot, featuring AI-generated responses",
+        'Suitable for up to 10,000 chat messages.',
         'Manage 2 Bot Profiles with 2 Knowledge Bases.',
-        'Text uploads allowed for content.',
+        'Text , PNG , JPEG uploads allowed for content.',
       ],
       backgroundColor: 'bg-indigo-500',
     },
     {
-      title: 'Starter',
-      price: '34.99',
-      sessions: '20,000 Sessions',
+      title: 'BotWot Pro',
+      price: '59.99',
+      sessions: '20,000 Messages',
       features: [
-        'Enhanced features for more extensive chatbot needs.',
-        'Suitable for up to 20,000 chat messages per month.',
-        'Manage 3 Bot Profiles with 4 Knowledge Bases.',
-        'Text, PNG, and JPEG uploads allowed.',
+        'Enhanced features for extensive chatbot needs with AI-generated responses.',
+        'Suitable for up to 20,000 chat messages.',
+        'Manage 4 Bot Profiles with 4 Knowledge Bases.',
+        'Text, PNG, JPEG, PDF, Excel, and Word uploads allowed.',
       ],
       backgroundColor: 'bg-fuchsia-950',
     },
-    {
-      title: 'Professional',
-      price: '69.99',
-      sessions: '50,000 Sessions',
-      features: [
-        'Inclusive tools for high-volume chatbot interactions.',
-        'Suitable for up to 50,000 chat messages per month.',
-        'Manage 5 Bot Profiles with 6 Knowledge Bases.',
-        'Text, PNG, JPEG, PDF, Excel, and Word uploads allowed.',
-      ],
-      backgroundColor: 'bg-pink-400',
-    },
+    // {
+    //   title: 'Professional',
+    //   price: '69.99',
+    //   sessions: '50,000 Sessions',
+    //   features: [
+    //     'Inclusive tools for high-volume chatbot interactions.',
+    //     'Suitable for up to 50,000 chat messages per month.',
+    //     'Manage 5 Bot Profiles with 6 Knowledge Bases.',
+    //     'Text, PNG, JPEG, PDF, Excel, and Word uploads allowed.',
+    //   ],
+    //   backgroundColor: 'bg-pink-400',
+    // },
   ];
 
   const handleBasicFreeTrial = () => {
@@ -232,7 +233,7 @@ const PricingCard = () => {
                 ) : (
                   <button
                     className="py-2 px-6 text-base font-medium bg-gray-100 rounded-lg text-slate-950 w-full"
-                    onClick={() => router.push('/login')}
+                    onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}
                   >
                     Start Free Trial
                   </button>
