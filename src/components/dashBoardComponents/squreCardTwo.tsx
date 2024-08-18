0b7be6cfaaf16515489e4dd18f1521e3186504f0
kalpanathmajhi <kalpanathmajhi@gmail.com>
2024-08-18 17:35:13 +0530

    added bot name in dashboard

--- src/components/dashBoardComponents/squreCardTwo.tsx
+++ src/components/dashBoardComponents/squreCardTwo.tsx
@@ -8,6 +8,7 @@ interface BotResult {
   messageCount: number;
   botId: {
     botId: string;
+    botName: string[];
     createdAt: string;
   };
 }
@@ -33,34 +34,46 @@ interface Metrics {
 export function SqureCardTwo() {
   const metrics = useSelector((state: RootState) => state.root.userMetric.data);
 
-  // Group the results by botId and formatted date
+  // Create a mapping from botId to botName
+  const botNameMap: { [key: string]: string[] } = {};
+
+  metrics?.results?.forEach((result: { botId: { botId: any; botName: any; }; }) => {
+    const botId = result.botId.botId;
+    const botName = result.botId.botName;
+
+    if (!botNameMap[botId] && botName.length > 0) {
+      botNameMap[botId] = botName;
+    }
+  });
+
+  // Group the results by botName and formatted date
   const groupedData: { [key: string]: { [key: string]: number } } = {};
 
-  metrics?.results?.forEach((result: { botId: { createdAt: string | number | Date; botId: any; }; messageCount: number; }) => {
+  metrics?.results?.forEach((result: { botId: { createdAt: string | number | Date; botId: string | number; }; messageCount: number; }) => {
     const formattedDate = format(new Date(result.botId.createdAt), 'MMM dd');
-    const botId = result?.botId.botId;
+    const botNames = botNameMap[result.botId.botId] || [`Unknown (${result.botId.botId})`];
 
-    if (!groupedData[formattedDate]) {
-      groupedData[formattedDate] = {};
-    }
+    botNames.forEach((botName) => {
+      if (!groupedData[formattedDate]) {
+        groupedData[formattedDate] = {};
+      }
 
-    if (!groupedData[formattedDate][botId]) {
-      groupedData[formattedDate][botId] = 0;
-    }
+      if (!groupedData[formattedDate][botName]) {
+        groupedData[formattedDate][botName] = 0;
+      }
 
-    groupedData[formattedDate][botId] += result.messageCount;
+      groupedData[formattedDate][botName] += result.messageCount;
+    });
   });
 
-  const chartdata = Object.keys(groupedData)?.map((date) => {
-    return { date, ...groupedData[date] };
-  });
+  const chartdata = Object.keys(groupedData).map((date) => ({
+    date,
+    ...groupedData[date],
+  }));
 
-
-  const categories:any = Array.from(
-    new Set(metrics?.results?.map((result: { botId: { botId: any; }; }) => result?.botId.botId))
+  const categories: string[] = Array.from(
+    new Set(metrics?.results.flatMap((result: { botId: { botId: string | number; }; }) => botNameMap[result.botId.botId] || []))
   );
-
-
 
   return (
     <>
