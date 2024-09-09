7987d203aee4107336f6c4d8a36f28e14f246c0b
sudhanshu-niva <Sudhanshu.batra@nivamoney.com>
2024-09-09 23:14:55 +0530

    deletion page

--- src/pages/data-deletion.tsx
+++ src/pages/data-deletion.tsx
@@ -1,7 +1,12 @@
 'use client';
+import Toast, { notifySuccess } from '@/components/Toaster/toast';
+import axiosInstance from '@/utils/axiosConfig';
+import axios from 'axios';
 import React, { useState } from 'react';
+
 interface FormData {
   reason: string;
+  customReason: string;
   email: string;
   otp: string;
   phoneNumber: string;
@@ -11,17 +16,70 @@ const DataDeletionPage: React.FC = () => {
   const [step, setStep] = useState(1);
   const [formData, setFormData] = useState<FormData>({
     reason: '',
+    customReason: '',
     email: '',
     otp: '',
     phoneNumber: '',
   });
 
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
     const { name, value } = e.target;
     setFormData((prevData) => ({ ...prevData, [name]: value }));
   };
 
+  const reasonsData = [
+    'Privacy Concerns',
+    'No Longer Needed',
+    'Difficult to Use',
+    'Technical Issues',
+    'Service Quality',
+    'Better Alternatives',
+    'Cost',
+    'Account Security',
+    'Personal Reasons',
+    'Not Valuable',
+    'Inactivity',
+    'User Experience',
+    'Other', // Adding "Other" option
+  ];
+
+  const verifyEmail = async () => {
+    try {
+      const response = await axiosInstance.post('/user/data-deletion/emailVerify', {
+        emailId: formData.email,
+      });
+      notifySuccess('OTP sent on registered email');
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+
+  const verifyOTP = async () => {
+    try {
+      const response = await axiosInstance.post('/user/data-deletion/otpVerify', {
+        emailId: formData.email,
+        otp: formData.otp,
+      });
+      console.log('Response:', response.data);
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+
   const handleNext = () => {
+    switch (step) {
+      case 1:
+        verifyEmail();
+        break;
+      case 2:
+        verifyOTP();
+        break;
+      case 3:
+        console.log('step', step);
+        break;
+      default:
+        break;
+    }
     setStep((prevStep) => prevStep + 1);
   };
 
@@ -36,10 +94,7 @@ const DataDeletionPage: React.FC = () => {
       case 1:
         return (
           <>
-            <label
-              htmlFor="email"
-              className="block text-sm font-medium text-gray-200 mb-1"
-            >
+            <label htmlFor="email" className="block text-sm font-medium text-gray-200 mb-1">
               Email
             </label>
             <input
@@ -49,17 +104,14 @@ const DataDeletionPage: React.FC = () => {
               value={formData.email}
               onChange={handleInputChange}
               className="w-full px-3 py-2 bg-gray-800 text-white rounded-md"
-              placeholder="please enter email"
+              placeholder="Please enter email"
             />
           </>
         );
       case 2:
         return (
           <>
-            <label
-              htmlFor="otp"
-              className="block text-sm font-medium text-gray-200 mt-4 mb-1"
-            >
+            <label htmlFor="otp" className="block text-sm font-medium text-gray-200 mt-4 mb-1">
               OTP
             </label>
             <input
@@ -76,10 +128,7 @@ const DataDeletionPage: React.FC = () => {
       case 3:
         return (
           <>
-            <label
-              htmlFor="phoneNumber"
-              className="block text-sm font-medium text-gray-200 mb-1"
-            >
+            <label htmlFor="phoneNumber" className="block text-sm font-medium text-gray-200 mb-1">
               Phone Number
             </label>
             <input
@@ -89,29 +138,46 @@ const DataDeletionPage: React.FC = () => {
               value={formData.phoneNumber}
               onChange={handleInputChange}
               className="w-full px-3 py-2 bg-gray-800 text-white rounded-md"
-              placeholder="johndoe@email.com"
+              placeholder="Enter phone number"
             />
-            <label
-              htmlFor="reason"
-              className="block text-sm font-medium text-gray-200 mb-1"
-            >
+            <label htmlFor="reason" className="block text-sm font-medium text-gray-200 mb-1">
               Reason
             </label>
             <select
               id="reason"
               name="reason"
               value={formData.reason}
-              // onChange={(e) => handleInputChange(e)}
+              onChange={handleInputChange}
               className="w-full px-3 py-2 bg-gray-800 text-white rounded-md"
             >
               <option value="">Select a reason</option>
-              <option value="privacy concerns">
-                <div className="flex flex-col">
-                  <span>Privacy Concerns</span>
-                  <span>I’m worried about how my data is used</span>
-                </div>
-              </option>
+              {reasonsData.map((item) => (
+                <option key={item} value={item}>
+                  {item}
+                </option>
+              ))}
             </select>
+
+            {/* Conditional rendering of input box if "Other" is selected */}
+            {formData.reason === 'Other' && (
+              <>
+                <label
+                  htmlFor="customReason"
+                  className="block text-sm font-medium text-gray-200 mt-4 mb-1"
+                >
+                  Please specify your reason
+                </label>
+                <input
+                  type="text"
+                  id="customReason"
+                  name="customReason"
+                  value={formData.customReason}
+                  onChange={handleInputChange}
+                  className="w-full px-3 py-2 bg-gray-800 text-white rounded-md"
+                  placeholder="Enter your reason"
+                />
+              </>
+            )}
           </>
         );
       default:
@@ -122,19 +188,16 @@ const DataDeletionPage: React.FC = () => {
   return (
     <div className="flex items-center justify-center min-h-screen">
       <div className="w-full max-w-md bg-black p-8 rounded-lg shadow-lg">
-        <h2 className="text-2xl font-bold text-white mb-6">
-          Account Deletion Form
-        </h2>
-        <p className="text-gray-300 mb-6">
-          Please fill out this form with the required information
-        </p>
+        <h2 className="text-2xl font-bold text-white mb-6">Account Deletion Form</h2>
+        <p className="text-gray-300 mb-6">Please fill out this form with the required information</p>
         <div className="flex justify-between mb-6">
           {[1, 2, 3].map((num) => (
             <div
               key={num}
-              className={`w-8 h-8 rounded-full flex items-center justify-center ${
+              className={`w-8 h-8 rounded-full flex items-center justify-center cursor-pointer ${
                 num === step ? 'bg-purple-500' : 'bg-gray-700'
               }`}
+              onClick={() => num <= step && setStep(num)}
             >
               {num}
             </div>
