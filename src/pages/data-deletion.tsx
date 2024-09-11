2d86c30dcb898a6eccc16bf10347702d4a5f667a
sudhanshu-niva <sudhanshu@nivamoney.com>
2024-09-11 23:00:20 +0530

    user deletion form

--- src/pages/data-deletion.tsx
+++ src/pages/data-deletion.tsx
@@ -1,7 +1,6 @@
 'use client';
 import Toast, { notifySuccess } from '@/components/Toaster/toast';
 import axiosInstance from '@/utils/axiosConfig';
-import axios from 'axios';
 import React, { useState } from 'react';
 
 interface FormData {
@@ -9,22 +8,23 @@ interface FormData {
   customReason: string;
   email: string;
   otp: string;
-  phoneNumber: string;
-  userId:string
+  userId: string;
 }
 
 const DataDeletionPage: React.FC = () => {
   const [step, setStep] = useState(1);
+  const [deletedSuccess, setDeletedSuccess] = useState(false);
   const [formData, setFormData] = useState<FormData>({
     reason: '',
     customReason: '',
     email: '',
     otp: '',
-    phoneNumber: '',
-    userId:''
+    userId: '',
   });
 
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+  const handleInputChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
+  ) => {
     const { name, value } = e.target;
     setFormData((prevData) => ({ ...prevData, [name]: value }));
   };
@@ -47,9 +47,13 @@ const DataDeletionPage: React.FC = () => {
 
   const verifyEmail = async () => {
     try {
-      const response = await axiosInstance.post('/user/data-deletion/emailVerify', {
-        emailId: formData.email,
-      });
+      const response = await axiosInstance.post(
+        '/user/data-deletion/emailVerify',
+        {
+          emailId: formData.email,
+        }
+      );
+      setStep((prevStep) => prevStep + 1);
       notifySuccess('OTP sent on registered email');
     } catch (error) {
       console.error('Error:', error);
@@ -58,22 +62,36 @@ const DataDeletionPage: React.FC = () => {
 
   const verifyOTP = async () => {
     try {
-      const response = await axiosInstance.post('/user/data-deletion/otpVerify', {
-        emailId: formData.email,
-        otp: formData.otp,
-      });
-      console.log('Response:', response.data);
+      const response = await axiosInstance.post(
+        '/user/data-deletion/otpVerify',
+        {
+          emailId: formData.email,
+          otp: formData.otp,
+        }
+      );
+      setStep((prevStep) => prevStep + 1);
+
+      setFormData((prevData) => ({
+        ...prevData,
+        userId: response.data.user_id,
+      }));
     } catch (error) {
       console.error('Error:', error);
     }
   };
   const deleteUser = async () => {
     try {
-      const response = await axiosInstance.post(`/user/deleteUser/${formData.userId}`, {
-        reason: formData.reason,
-        otp: formData.reason==='Other'?formData.customReason:formData.reason,
-      });
-      console.log('Response:', response.data);
+      const response = await axiosInstance.post(
+        `/user/deleteUser/${formData.userId}`,
+        {
+          reason: formData.reason,
+          otp:
+            formData.reason === 'Other'
+              ? formData.customReason
+              : formData.reason,
+        }
+      );
+      setDeletedSuccess(true);
     } catch (error) {
       console.error('Error:', error);
     }
@@ -87,19 +105,14 @@ const DataDeletionPage: React.FC = () => {
       case 2:
         verifyOTP();
         break;
-      case 3:
-        console.log('step', step);
-        break;
       default:
         break;
     }
-    setStep((prevStep) => prevStep + 1);
   };
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
-    // Handle form submission logic here
-    console.log('Form submitted:', formData);
+    deleteUser();
   };
 
   const renderStep = () => {
@@ -107,7 +120,10 @@ const DataDeletionPage: React.FC = () => {
       case 1:
         return (
           <>
-            <label htmlFor="email" className="block text-sm font-medium text-gray-200 mb-1">
+            <label
+              htmlFor="email"
+              className="block text-sm font-medium text-gray-200 mb-1"
+            >
               Email
             </label>
             <input
@@ -124,7 +140,10 @@ const DataDeletionPage: React.FC = () => {
       case 2:
         return (
           <>
-            <label htmlFor="otp" className="block text-sm font-medium text-gray-200 mt-4 mb-1">
+            <label
+              htmlFor="otp"
+              className="block text-sm font-medium text-gray-200 mt-4 mb-1"
+            >
               OTP
             </label>
             <input
@@ -141,19 +160,10 @@ const DataDeletionPage: React.FC = () => {
       case 3:
         return (
           <>
-            <label htmlFor="phoneNumber" className="block text-sm font-medium text-gray-200 mb-1">
-              Phone Number
-            </label>
-            <input
-              type="tel"
-              id="phoneNumber"
-              name="phoneNumber"
-              value={formData.phoneNumber}
-              onChange={handleInputChange}
-              className="w-full px-3 py-2 bg-gray-800 text-white rounded-md"
-              placeholder="Enter phone number"
-            />
-            <label htmlFor="reason" className="block text-sm font-medium text-gray-200 mb-1">
+            <label
+              htmlFor="reason"
+              className="block text-sm font-medium text-gray-200 mb-1"
+            >
               Reason
             </label>
             <select
@@ -171,7 +181,6 @@ const DataDeletionPage: React.FC = () => {
               ))}
             </select>
 
-            {/* Conditional rendering of input box if "Other" is selected */}
             {formData.reason === 'Other' && (
               <>
                 <label
@@ -201,42 +210,55 @@ const DataDeletionPage: React.FC = () => {
   return (
     <div className="flex items-center justify-center min-h-screen">
       <div className="w-full max-w-md bg-black p-8 rounded-lg shadow-lg">
-        <h2 className="text-2xl font-bold text-white mb-6">Account Deletion Form</h2>
-        <p className="text-gray-300 mb-6">Please fill out this form with the required information</p>
-        <div className="flex justify-between mb-6">
-          {[1, 2, 3].map((num) => (
-            <div
-              key={num}
-              className={`w-8 h-8 rounded-full flex items-center justify-center cursor-pointer ${
-                num === step ? 'bg-purple-500' : 'bg-gray-700'
-              }`}
-              onClick={() => num <= step && setStep(num)}
-            >
-              {num}
+        {!deletedSuccess ? (
+          <>
+            <h2 className="text-2xl font-bold text-white mb-6">
+              Account Deletion Form
+            </h2>
+
+            <p className="text-gray-300 mb-6">
+              Please fill out this form with the required information
+            </p>
+            <div className="flex justify-between mb-6">
+              {[1, 2, 3].map((num) => (
+                <div
+                  key={num}
+                  className={`w-8 h-8 rounded-full flex items-center justify-center cursor-pointer ${
+                    num === step ? 'bg-purple-500' : 'bg-gray-700'
+                  }`}
+                  onClick={() => num <= step && setStep(num)}
+                >
+                  {num}
+                </div>
+              ))}
             </div>
-          ))}
-        </div>
-        <form onSubmit={handleSubmit}>
-          {renderStep()}
-          <div className="mt-6">
-            {step < 3 ? (
-              <button
-                type="button"
-                onClick={handleNext}
-                className="w-full bg-purple-500 text-white py-2 px-4 rounded-md hover:bg-purple-600 transition-colors"
-              >
-                Next
-              </button>
-            ) : (
-              <button
-                type="submit"
-                className="w-full bg-purple-500 text-white py-2 px-4 rounded-md hover:bg-purple-600 transition-colors"
-              >
-                Done
-              </button>
-            )}
+            <form onSubmit={handleSubmit}>
+              {renderStep()}
+              <div className="mt-6">
+                {step < 3 ? (
+                  <button
+                    type="button"
+                    onClick={handleNext}
+                    className="w-full bg-purple-500 text-white py-2 px-4 rounded-md hover:bg-purple-600 transition-colors"
+                  >
+                    Next
+                  </button>
+                ) : (
+                  <button
+                    type="submit"
+                    className="w-full bg-purple-500 text-white py-2 px-4 rounded-md hover:bg-purple-600 transition-colors"
+                  >
+                    Done
+                  </button>
+                )}
+              </div>
+            </form>
+          </>
+        ) : (
+          <div className="text-2xl font-bold text-white">
+            Your account has been deleted successfully.
           </div>
-        </form>
+        )}
       </div>
     </div>
   );
