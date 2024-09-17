697c1990a6b7488dcc6ae464d9ecbaa066e84b14
kalpanathmajhi <kalpanathmajhi@gmail.com>
2024-09-17 11:34:42 +0530

    added new cards

--- src/app/integration/cards.tsx
+++ src/app/integration/cards.tsx
@@ -9,7 +9,7 @@ interface CardProps {
 
 const Card: React.FC<CardProps> = ({ image, title, subTitle, description }) => {
     return (
-        <div className="bg-black text-white rounded-lg p-8 w-96 shadow-lg border border-gray-500">
+        <div className="bg-black text-white rounded-lg p-6 md:p-8 w-full sm:w-80 md:w-96 shadow-lg border border-gray-500 mx-auto my-4">
             <div className="flex items-center mb-4">
                 <div className="ml-4">
                     <h3 className="text-lg font-semibold">{title}</h3>
@@ -22,21 +22,20 @@ const Card: React.FC<CardProps> = ({ image, title, subTitle, description }) => {
                 </div>
             </div>
             <div className="flex justify-center mb-4">
-                <img src={image} alt={`${title} icon`} className="h-24" />
+                <img src={image} alt={`${title} icon`} className="h-24 object-contain" />
             </div>
 
             <p className="text-sm text-gray-300">{description}</p>
             <div className="flex justify-between mt-4">
                 <button
-                    className="text-white px-6 py-3 rounded-full border-2 border-white bg-transparent"
+                    className="text-white px-4 py-2 md:px-6 md:py-3 rounded-full border-2 border-white bg-transparent"
                 >
                     Export
                 </button>
 
-                <button className="text-white px-6 py-3 rounded-full" style={{ backgroundColor: '#3F2181' }}>
-                    Enabled
+                <button className="text-white px-4 py-2 md:px-6 md:py-3 rounded-full" style={{ backgroundColor: '#3F2181' }}>
+                    Contact support
                 </button>
-
             </div>
         </div>
     );
