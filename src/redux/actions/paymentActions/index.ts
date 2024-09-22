155e5ee0e837d9a6c7296d102203d1aeceb4c0a3
sneha-jhaveri <sneha@nivamoney.com>
2024-09-23 04:18:25 +0530

    capture api changes

--- src/redux/actions/paymentActions/index.ts
+++ src/redux/actions/paymentActions/index.ts
@@ -1,22 +1,22 @@
-import { CREATE_PAYMENT_REQUEST, CREATE_PAYMENT_SUCCESS, CREATE_PAYMENT_FAILURE, CAPTURE_PAYMENT_REQUEST, CAPTURE_PAYMENT_SUCCESS, CAPTURE_PAYMENT_FAILURE } from "../actionTypes";
+import { CREATE_PAYMENT_REQUEST, CREATE_PAYMENT_SUCCESS, CREATE_PAYMENT_FAILURE, CAPTURE_PAYMENT_REQUEST, CAPTURE_PAYMENT_SUCCESS, CAPTURE_PAYMENT_FAILURE, FETCH_PLANS_SUCCESS, FETCH_PLANS, FETCH_PLANS_FAILURE } from "../actionTypes";
 
 export const fetchPlans = () => ({
-  type: 'FETCH_PLANS'
+  type: FETCH_PLANS
 });
 
 export const fetchPlansSuccess = (plans : any) => ({
-  type: 'FETCH_PLANS_SUCCESS',
+  type: FETCH_PLANS_SUCCESS,
   payload: plans
 });
 
 export const fetchPlansFailure = (error : any) => ({
-  type: 'FETCH_PLANS_FAILURE',
+  type: FETCH_PLANS_FAILURE,
   payload: error
 });
 
 
 export const createPaymentRequest = (payload: { planId: string; data: any }) => ({
-  type: 'CREATE_PAYMENT_REQUEST',
+  type: CREATE_PAYMENT_REQUEST,
   payload
 });
 
