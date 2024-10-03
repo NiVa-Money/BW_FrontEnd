628dd563611c93e04a31e5b6c5c52f25798541fd
sneha-jhaveri <sneha@nivamoney.com>
2024-10-04 02:59:35 +0530

    fixed minor changes

--- src/components/dashBoardComponents/ChartCardOne.tsx
+++ src/components/dashBoardComponents/ChartCardOne.tsx
@@ -1,222 +1,31 @@
-// import React, { useState } from 'react';
-// import { Card, Title, AreaChart, BarChart, Metric, Text } from '@tremor/react';
-// import ArrowUpward from '@mui/icons-material/ArrowUpward';
-// import { Select, MenuItem } from '@mui/material';
-// import { RootState } from '@/redux/configureStore';
-// import { useSelector } from 'react-redux';
-// import { format } from 'date-fns';
-
-// const resolvedData = [
-//   { time: '12 AM', Bot1: 40, Bot2: 60 },
-//   { time: '4 AM', Bot1: 30, Bot2: 40 },
-//   { time: '8 AM', Bot1: 50, Bot2: 70 },
-//   { time: '12 PM', Bot1: 40, Bot2: 50 },
-//   { time: '4 PM', Bot1: 60, Bot2: 80 },
-//   { time: '8 PM', Bot1: 50, Bot2: 60 },
-// ];
-
-// const npsData = [
-//   { time: '12 AM', nps: 50 },
-//   { time: '4 AM', nps: 150 },
-//   { time: '8 AM', nps: 100 },
-//   { time: '12 PM', nps: 250 },
-//   { time: '4 PM', nps: 180 },
-//   { time: '8 PM', nps: 100 },
-// ];
-
-// const ChartCardOne = () => {
-//   const [dateRange, setDateRange] = useState('Jan 2024 - Dec 2024');
-
-//   const metrics = useSelector(
-//     (state: RootState) => state.root?.userMetric?.data
-//   );
-
-//   // Create a mapping from botId to botName
-//   const botNameMap: { [key: string]: string[] } = {};
-
-//   metrics?.results?.forEach(
-//     (result: { botId: { botId: any; botName: any } }) => {
-//       const botId = result?.botId?.botId;
-//       const botName = result?.botId?.botName;
-
-//       if (!botNameMap[botId] && botName?.length > 0) {
-//         botNameMap[botId] = botName;
-//       }
-//     }
-//   );
-
-//   // Group the results by botName and formatted date
-//   const groupedData: { [key: string]: { [key: string]: number } } = {};
-
-//   metrics?.results?.forEach(
-//     (result: {
-//       botId: { createdAt: string | number | Date; botId: string | number };
-//       messageCount: number;
-//     }) => {
-//       const formattedDate = format(
-//         new Date(result?.botId?.createdAt),
-//         'MMM dd'
-//       );
-//       const botNames = botNameMap[result?.botId?.botId] || [
-//         `Unknown (${result.botId.botId})`,
-//       ];
-
-//       botNames.forEach((botName) => {
-//         if (!groupedData[formattedDate]) {
-//           groupedData[formattedDate] = {};
-//         }
-
-//         if (!groupedData[formattedDate][botName]) {
-//           groupedData[formattedDate][botName] = 0;
-//         }
-//         groupedData[formattedDate][botName] += result.messageCount;
-//       });
-//     }
-//   );
-
-//   const chartdata = Object.keys(groupedData).map((date) => ({
-//     date,
-//     ...groupedData[date],
-//   }));
-
-//   const categories: string[] = Array.from(
-//     new Set(
-//       metrics?.results?.flatMap(
-//         (result: { botId: { botId: string | number } }) =>
-//           botNameMap[result?.botId?.botId] || []
-//       )
-//     )
-//   );
-
-//   return (
-//     <div className="bg-[#0B031E] p-6">
-//       <div className="flex gap-6">
-//         {/* Bot Usage Chart */}
-//         <Card className="flex-1 !bg-white !bg-opacity-10 !border-r-[1px] border-none mb-6 h-auto" >
-//           <div className="flex justify-between items-start mb-4">
-//             <div>
-//               <Title className="text-xl text-[#AEB9E1]">Bot Usage</Title>
-//               <Metric className="text-3xl font-bold text-white mt-2">
-//                 $240.8K
-//               </Metric>
-//               <Text className="text-green-400 flex items-center text-sm mt-1">
-//                 <ArrowUpward className="w-4 h-4 mr-1" />
-//                 24.6%
-//               </Text>
-//             </div>
-//             <Text className="text-sm text-[#AEB9E1]">
-//               <Select
-//                 value={dateRange}
-//                 onChange={(e) => setDateRange(e.target.value)}
-//                 className="mb-4 bg-[#0A1330] text-white"
-//                 style={{ color: 'white', borderColor: '#343B4F' }}
-//               >
-//                 <MenuItem value="Jan 2024 - Dec 2024">
-//                   Jan 2024 - Dec 2024
-//                 </MenuItem>
-//                 <MenuItem value="Jan 2023 - Dec 2023">
-//                   Jan 2023 - Dec 2023
-//                 </MenuItem>
-//               </Select>
-//             </Text>
-//           </div>
-//           <div className="h-[500px] w-[600px]">
-//             <AreaChart
-//               className="h-full w-full"
-//               data={chartdata}
-//               categories={categories}
-//               index="date"
-//               colors={['purple', 'cyan']}
-//             />
-//           </div>
-//         </Card>
-
-//         {/* Unresolved/Resolved and NPS Section */}
-//         <div className="flex flex-1 flex-col gap-6">
-//           {/* Unresolved/Resolved Chart */}
-//           <Card className="flex-grow border-none" style={{ backgroundColor: 'rgba(255, 255, 255, 0.1)' }}>
-//             <div className="flex justify-between items-start mb-4">
-//               <div>
-//                 <Title className="text-xl text-[#AEB9E1]">
-//                   Unresolved/Resolved
-//                 </Title>
-//                 <Metric className="text-3xl font-bold text-white mt-2">
-//                   $144.6K
-//                 </Metric>
-//                 <Text className="text-green-400 flex items-center text-sm mt-1">
-//                   <ArrowUpward className="w-4 h-4 mr-1" />
-//                   28.5%
-//                 </Text>
-//               </div>
-//             </div>
-//             <div className="h-[300px] w-full">
-//               <BarChart
-//                 className="h-full w-full"
-//                 data={resolvedData}
-//                 index="time"
-//                 categories={['Bot1', 'Bot2']}
-//                 colors={['purple', 'cyan']}
-//               />
-//             </div>
-//           </Card>
-
-//           {/* NPS Chart */}
-//           <Card className="flex-grow bg-white bg-opacity-10 border-none" style={{ backgroundColor: 'rgba(255, 255, 255, 0.1)' }}>
-//             <div className="flex justify-between items-start mb-4">
-//               <div>
-//                 <Title className="text-xl text-[#AEB9E1]">
-//                   Net Promoter Score (NPS)
-//                 </Title>
-//                 <Metric className="text-3xl font-bold text-white mt-2">
-//                   92%
-//                 </Metric>
-//                 <Text className="text-green-400 flex items-center text-sm mt-1">
-//                   <ArrowUpward className="w-4 h-4 mr-1" />
-//                   15.9%
-//                 </Text>
-//               </div>
-//             </div>
-//             <div className="h-[300px] w-full">
-//               <AreaChart
-//                 className="h-full w-full"
-//                 data={npsData}
-//                 index="time"
-//                 categories={['nps']}
-//                 colors={['purple']}
-//               />
-//             </div>
-//           </Card>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default ChartCardOne;
-
 import React, { useState } from 'react';
 import { Card, Title, AreaChart, BarChart, Metric, Text } from '@tremor/react';
 import ArrowUpward from '@mui/icons-material/ArrowUpward';
-import { Select, MenuItem } from '@mui/material';
 import { RootState } from '@/redux/configureStore';
 import { useSelector } from 'react-redux';
 import { format } from 'date-fns';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 
 const ChartCardOne = () => {
-  const [dateRange, setDateRange] = useState('Jan 2024 - Dec 2024');
-
   // Get the metrics data from the Redux store
   const metrics = useSelector(
     (state: RootState) => state.root?.userMetric?.data
   );
-  
+
   // Extract resolved and unresolved session data from metrics
   const resolvedSessions = metrics?.resolvedSessions ?? 0;
   const unresolvedSessions = metrics?.unresolvedSessions ?? 0;
 
   // Data for the Unresolved/Resolved Bar Chart
   const resolvedData = [
-    { time: 'Current', Resolved: resolvedSessions, Unresolved: unresolvedSessions },
+    {
+      time: 'Current',
+      Resolved: resolvedSessions,
+      Unresolved: unresolvedSessions,
+    },
   ];
 
   const npsData = [
@@ -292,29 +101,29 @@ const ChartCardOne = () => {
             <div className="flex justify-between items-start mb-4">
               <div>
                 <Title className="text-xl text-[#AEB9E1]">Bot Usage</Title>
-                <Metric className="text-3xl font-bold text-white mt-2">
+                {/* <Metric className="text-3xl font-bold text-white mt-2">
                   $240.8K
-                </Metric>
-                <Text className="text-green-400 flex items-center text-sm mt-1">
-                  <ArrowUpward className="w-4 h-4 mr-1" />
-                  24.6%
-                </Text>
+                </Metric> */}
               </div>
-              <Text className="text-sm text-[#AEB9E1]">
-                <Select
-                  value={dateRange}
-                  onChange={(e) => setDateRange(e.target.value)}
-                  className="bg-[#0A1330] text-[#AEB9E1]"
-                  style={{ color: '#AEB9E1', borderColor: '#343B4F' }}
-                >
-                  <MenuItem value="Jan 2024 - Dec 2024">
-                    Jan 2024 - Dec 2024
-                  </MenuItem>
-                  <MenuItem value="Jan 2023 - Dec 2023">
-                    Jan 2023 - Dec 2023
-                  </MenuItem>
-                </Select>
-              </Text>
+              <LocalizationProvider dateAdapter={AdapterDayjs}>
+                <DemoContainer components={['DatePicker']}>
+                  <DatePicker
+                    label="Select date"
+                    sx={{
+                      backgroundColor: '#0A1330',
+                      '& .MuiInputBase-root': {
+                        color: '#AEB9E1',
+                      },
+                      '& .MuiInputLabel-root': {
+                        color: '#AEB9E1',
+                      },
+                      '& .MuiSvgIcon-root': {
+                        color: '#AEB9E1',
+                      },
+                    }}
+                  />
+                </DemoContainer>
+              </LocalizationProvider>
             </div>
             <div className="h-[500px] w-full">
               <AreaChart
@@ -336,13 +145,9 @@ const ChartCardOne = () => {
                 <Title className="text-xl text-[#AEB9E1]">
                   Unresolved/Resolved
                 </Title>
-                <Metric className="text-3xl font-bold text-white mt-2">
+                {/* <Metric className="text-3xl font-bold text-white mt-2">
                   $144.6K
-                </Metric>
-                <Text className="text-green-400 flex items-center text-sm mt-1">
-                  <ArrowUpward className="w-4 h-4 mr-1" />
-                  28.5%
-                </Text>
+                </Metric> */}
               </div>
             </div>
             <div className="h-[180px] w-full">
@@ -364,10 +169,6 @@ const ChartCardOne = () => {
                 <Metric className="text-3xl font-bold text-white mt-2">
                   92%
                 </Metric>
-                <Text className="text-green-400 flex items-center text-sm mt-1">
-                  <ArrowUpward className="w-4 h-4 mr-1" />
-                  15.9%
-                </Text>
               </div>
             </div>
             <div className="h-[180px] w-full">
--- src/components/dashBoardComponents/ChartCardTwo.tsx
+++ src/components/dashBoardComponents/ChartCardTwo.tsx
@@ -1,122 +1,12 @@
-// import React, { useState } from 'react';
-// import { Card, Title, BarChart, Metric } from '@tremor/react';
-// import { Select, MenuItem } from '@mui/material';
-// import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
-
-// const monthlyData = [
-//   { month: 'Jan', Positive: 20000, Neutral: 15000, Negative: 10000 },
-//   { month: 'Feb', Positive: 25000, Neutral: 50000, Negative: 15000 },
-//   { month: 'Mar', Positive: 35000, Neutral: 40000, Negative: 12000 },
-//   { month: 'Apr', Positive: 40000, Neutral: 30000, Negative: 5000 },
-//   { month: 'May', Positive: 30000, Neutral: 20000, Negative: 10000 },
-//   { month: 'Jun', Positive: 20000, Neutral: 50000, Negative: 15000 },
-//   { month: 'Jul', Positive: 15000, Neutral: 5000, Negative: 0 },
-//   { month: 'Aug', Positive: 40000, Neutral: 35000, Negative: 20000 },
-//   { month: 'Sep', Positive: 20000, Neutral: 10000, Negative: 10000 },
-//   { month: 'Oct', Positive: 30000, Neutral: 20000, Negative: 15000 },
-//   { month: 'Nov', Positive: 25000, Neutral: 0, Negative: 20000 },
-//   { month: 'Dec', Positive: 30000, Neutral: 20000, Negative: 25000 },
-// ];
-
-// const bots = [
-//   { name: 'Bot 1', messages: 1230, color: 'bg-purple-500' },
-//   { name: 'Bot 2', messages: 751, color: 'bg-cyan-500' },
-//   { name: 'Bot 3', messages: 471, color: 'bg-blue-500' },
-//   { name: 'Bot 4', messages: 280, color: 'bg-cyan-500' },
-//   { name: 'Bot 5', messages: 87, color: 'bg-blue-500' },
-// ];
-
-// const ReportsOverview = () => {
-//   const [dateRange, setDateRange] = useState('Jan 2024 - Dec 2024');
-//   const [sentimentPeriod, setSentimentPeriod] = useState('Monthly'); // Manage state for dropdown
-
-//   return (
-//     <div className="bg-[#0B031E] p-4">
-//       <div className="flex-col mb-4">
-//         <h1 className="text-white text-2xl font-semibold mb-4">
-//           Reports overview
-//         </h1>
-//         <Select
-//           value={dateRange}
-//           onChange={(e) => setDateRange(e.target.value)}
-//           className="mb-4 bg-[#0A1330] text-white"
-//           style={{ color: 'white', borderColor: '#343B4F' }}
-//         >
-//           <MenuItem value="Jan 2024 - Dec 2024">Jan 2024 - Dec 2024</MenuItem>
-//           <MenuItem value="Jan 2023 - Dec 2023">Jan 2023 - Dec 2023</MenuItem>
-//         </Select>
-//       </div>
-
-//       <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-//         <Card className="!bg-opacity-10 !bg-white">
-//           <Title className="text-[#AEB9E1] mb-2">Total Messages</Title>
-//           <Metric className="text-white text-4xl font-bold">10,234</Metric>
-
-//           <div className="mt-4">
-//             <Title className="text-[#AEB9E1] mb-2">Top 5 Bots</Title>
-//             <div className="flex justify-between text-[#AEB9E1] mb-2">
-//               <span>Bots</span>
-//               <span>Messages</span>
-//             </div>
-//             {bots.map((bot, index) => (
-//               <div key={index} className="flex items-center mt-2">
-//                 <span className="text-[#AEB9E1] w-12">{bot.name}</span>
-//                 <div className="flex-grow mx-2">
-//                   <div
-//                     className={`h-2 ${bot.color} rounded-full`}
-//                     style={{
-//                       width: `${(bot.messages / bots[0].messages) * 100}%`,
-//                     }}
-//                   ></div>
-//                 </div>
-//                 <span className="text-white w-12 text-right">
-//                   {bot.messages}
-//                 </span>
-//               </div>
-//             ))}
-//           </div>
-//         </Card>
-
-//         <Card className="!bg-opacity-10 !bg-white">
-//           <div className="flex justify-between items-center mb-4">
-//             <div>
-//               <Title className="text-[#AEB9E1]">Sentiment Analysis</Title>
-//             </div>
-//             <Select
-//                value={sentimentPeriod}
-//               variant="outlined"
-//               className="bg-[#0A1330] text-[#AEB9E1]"
-//               IconComponent={KeyboardArrowDownIcon}
-//             >
-//               <MenuItem value="Monthly">Monthly</MenuItem>
-//               <MenuItem value="Weekly">Weekly</MenuItem>
-//               <MenuItem value="Yearly">Yearly</MenuItem>
-//             </Select>
-//           </div>
-//           <BarChart
-//             className="h-72 mt-4"
-//             data={monthlyData}
-//             index="month"
-//             categories={['Positive', 'Neutral', 'Negative']}
-//             colors={['purple', 'cyan', 'blue']}
-//             valueFormatter={(number) =>
-//               `${Intl.NumberFormat('us').format(number).toString()}K`
-//             }
-//             stack={true}
-//           />
-//         </Card>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default ReportsOverview;
-
-import React, { useState } from 'react';
+import React from 'react';
 import { Card, Title, BarChart, Metric } from '@tremor/react';
-import { Select, MenuItem } from '@mui/material';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { RootState } from '@/redux/configureStore';
 import { useSelector } from 'react-redux';
+import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import Multiselect from 'multiselect-react-dropdown';
 
 type BotSessionMappingType = [string, [number]][];
 
@@ -136,27 +26,27 @@ const monthlyData = [
 ];
 
 const ReportsOverview = () => {
-  const [dateRange, setDateRange] = useState('Jan 2024 - Dec 2024');
-  const [sentimentPeriod, setSentimentPeriod] = useState('Monthly');
-
   const metrics = useSelector(
     (state: RootState) => state.root?.userMetric?.data
   );
 
-  // Define the type for botSessionMapping and extract it from the metrics
   const botSessionMapping: BotSessionMappingType =
     metrics?.botSessionMapping || [];
   const totalMessages = metrics?.sessionConsumed || 0;
 
-  // Sort the botSessionMapping based on the message count in descending order
   const topBots = [...botSessionMapping]
     .sort((a, b) => b[1][0] - a[1][0])
     .slice(0, 5);
 
-  // Determine the highest message count for the progress bar percentage calculation
   const maxMessages = Math.max(
     ...botSessionMapping.map(([, [messages]]) => messages)
   );
+
+  const options = [
+    { name: 'Option 1', id: 1 },
+    { name: 'Option 2', id: 2 },
+    { name: 'Option 3', id: 3 },
+  ];
 
   return (
     <div className="bg-[#0B031E] p-4">
@@ -164,15 +54,57 @@ const ReportsOverview = () => {
         <h1 className="text-white text-2xl font-semibold mb-4">
           Reports overview
         </h1>
-        <Select
-          value={dateRange}
-          onChange={(e) => setDateRange(e.target.value)}
-          className="bg-[#0A1330] text-[#AEB9E1]"
-          style={{ color: '#AEB9E1', borderColor: '#343B4F' }}
-        >
-          <MenuItem value="Jan 2024 - Dec 2024">Jan 2024 - Dec 2024</MenuItem>
-          <MenuItem value="Jan 2023 - Dec 2023">Jan 2023 - Dec 2023</MenuItem>
-        </Select>
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+          <DemoContainer components={['DatePicker']}>
+            <DatePicker
+              label="Select date"
+              sx={{
+                backgroundColor: '#0A1330',
+                '& .MuiInputBase-root': {
+                  color: '#AEB9E1',
+                },
+                '& .MuiInputLabel-root': {
+                  color: '#AEB9E1',
+                },
+                '& .MuiSvgIcon-root': {
+                  color: '#AEB9E1',
+                },
+              }}
+            />
+            {/* <Multiselect
+              options={options} // Options for the dropdown
+              isObject={true}
+              displayValue="name" // Property to display in the dropdown
+              placeholder="Select bot"
+              onSelect={(selectedList, selectedItem) => {
+                console.log('Selected:', selectedList, selectedItem);
+              }}
+              onRemove={(selectedList, removedItem) => {
+                console.log('Removed:', selectedList, removedItem);
+              }}
+              style={{
+                multiselectContainer: {
+                  backgroundColor: '#0A1330',
+                },
+                searchBox: {
+                  backgroundColor: '#0A1330',
+                  color: '#AEB9E1',
+                },
+                chips: {
+                  backgroundColor: '#AEB9E1',
+                  color: 'black',
+                },
+                option: {
+                  color: '#AEB9E1',
+                  backgroundColor: '#0A1330',
+                  '&:hover': {
+                    backgroundColor: '#1A1A1A',
+                  },
+                },
+              }}
+            /> */}
+          </DemoContainer>
+        </LocalizationProvider>
       </div>
 
       <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
@@ -188,40 +120,28 @@ const ReportsOverview = () => {
               <span>Bots</span>
               <span>Messages</span>
             </div>
-            {topBots
-              .map(([botName, [messages]], index: number) => (
-                <div key={index} className="flex items-center mt-2">
-                  <span className="text-[#AEB9E1] w-12">{botName}</span>
-                  <div className="flex-grow mx-2">
-                    <div
-                      className={`h-2 bg-purple-600 rounded-full`}
-                      style={{
-                        width: `${(messages / maxMessages) * 100}%`,
-                      }}
-                    ></div>
-                  </div>
-                  <span className="text-white w-12 text-right">{messages}</span>
+            {topBots.map(([botName, [messages]], index: number) => (
+              <div key={index} className="flex items-center mt-2">
+                <span className="text-[#AEB9E1] w-20">{botName}</span>{' '}
+                {/* Adjusted width */}
+                <div className="flex-grow mx-2">
+                  <div
+                    className={`h-2 bg-purple-600 rounded-full`}
+                    style={{
+                      width: `${(messages / maxMessages) * 100}%`,
+                    }}
+                  ></div>
                 </div>
-              ))}
+                <span className="text-white w-12 text-right">{messages}</span>
+              </div>
+            ))}
           </div>
         </Card>
-
         <Card className="!bg-opacity-10 !bg-white">
           <div className="flex justify-between items-center mb-4">
             <div>
               <Title className="text-[#AEB9E1]">Sentiment Analysis</Title>
             </div>
-            <Select
-              value={sentimentPeriod}
-              onChange={(e) => setSentimentPeriod(e.target.value)}
-              variant="outlined"
-              className="bg-[#0A1330] text-[#AEB9E1]"
-              style={{ color: '#AEB9E1', borderColor: '#343B4F' }}
-            >
-              <MenuItem value="Monthly">Monthly</MenuItem>
-              <MenuItem value="Weekly">Weekly</MenuItem>
-              <MenuItem value="Yearly">Yearly</MenuItem>
-            </Select>
           </div>
           <BarChart
             className="h-72 mt-4"
