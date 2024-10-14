2dad342df847d4d40c8feb1e1b86ec8cec3f4492
kalpanathmajhi <kalpanathmajhi@gmail.com>
2024-10-15 00:34:49 +0530

    added ui fix in card and pricing

--- src/app/integration/page.tsx
+++ src/app/integration/page.tsx
@@ -14,21 +14,35 @@ const Integration = () => {
                 image: "/images/slack.png",
                 title: "Slack",
                 subTitle: "Integration",
-                description: "Integrate your calendar to streamline scheduling, manage appointments, and never miss an important event or meeting."
+                description: "Integrate your Slack to streamline scheduling, manage appointments, and never miss an important event or meeting."
             }
         ],
         [
             {
-                image: "/images/celanderImg.png",
-                title: "Calendar",
+                image: "/images/telegram.png",
+                title: "Telegram",
                 subTitle: "Integration",
-                description: "Integrate your Slack to streamline scheduling, manage appointments, and never miss an important event or meeting."
+                description: "Integrate your Telegram to streamline scheduling, manage appointments, and never miss an important event or meeting."
             },
             {
                 image: "/images/shopify.jpg",
                 title: "Shopify",
                 subTitle: "Integration",
                 description: "Integrate your Shopify to streamline scheduling, manage appointments, and never miss an important event or meeting."
+            }
+        ],
+        [
+            {
+                image: "/images/facebook.png",
+                title: "Facebook",
+                subTitle: "Integration",
+                description: "Integrate your Facebook to streamline scheduling, manage appointments, and never miss an important event or meeting."
+            },
+            {
+                image: "/images/instagram.png",
+                title: "Instagram",
+                subTitle: "Integration",
+                description: "Integrate your Instagram to streamline scheduling, manage appointments, and never miss an important event or meeting."
             }
         ]
     ];
