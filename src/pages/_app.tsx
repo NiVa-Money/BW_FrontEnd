e689ace725d5aecc6b06955ee833d2bf746bb210
sneha-jhaveri <sneha@nivamoney.com>
2024-10-22 23:44:35 +0530

    added scripts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -36,28 +36,32 @@ function MyApp({ Component, pageProps }: AppProps) {
             type="image/svg"
             href="@/public/assets/mainLogo.svg"
           />
-          <Script id="clarity-analytics" strategy="afterInteractive">
-            {`
-            (function(c,l,a,r,i,t,y){
-                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-            })(window, document, "clarity", "script", "oi04qe04w6");
-          `}
-          </Script>
-          <Script
-            async
-            src="https://www.googletagmanager.com/gtag/js?id=G-Z2ZBZZN0Y0"
-          />
-          <Script id="google-analytics" strategy="afterInteractive">
-            {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){ window.dataLayer.push(arguments); }
-            gtag('js', new Date());
-            gtag('config', 'G-Z2ZBZZN0Y0');
-          `}
-          </Script>
         </Head>
+
+        <Script type="text/javascript">
+          {`
+              (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "oi04qe04w6");
+          `}
+        </Script>
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-Z2ZBZZN0Y0"
+        ></Script>
+        <Script>
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            // Configure both Google Analytics and Google Ads
+            gtag('config', 'G-Z2ZBZZN0Y0');
+            gtag('config', 'AW-16484085944');
+          `}
+        </Script>
         <div className={`${inter.className} flex flex-col min-h-screen`}>
           <Header />
           <main className="flex-grow">
